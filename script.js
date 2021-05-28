a5566430de74bc961e27c102cd7b5d38861c7057
thomas005 <>
2021-05-28 16:24:48 +0000

    dark theme fixed

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 window.onload = function() {
   let btn = document.getElementById("dark-theme-btn")
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.className = "dark-theme";
+    btn.innerHTML = "🔆"
+  }
   btn.onclick = () => {
     if (document.body.className) {
       document.body.className = "";
       btn.innerHTML = "🌙"
+      localStorage.setItem("theme", "");
     } else {
       document.body.className = "dark-theme";
       btn.innerHTML = "🔆"
+      localStorage.setItem("theme", "dark");
     }
   }
 }
