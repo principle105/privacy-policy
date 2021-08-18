d63aad42822dfe1bd5f1f765e882b88c8854e553
Principle 105 <principle105@gmail.com>
2021-08-17 21:02:37 -0400

    fixed some formatting

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-window.onload = function() {
-  let btn = document.getElementById("dark-theme-btn")
-  let body = document.body
+window.onload = function () {
+    let btn = document.getElementById("dark-theme-btn");
+    let body = document.body;
 
-  if (localStorage.getItem("theme") === "dark") {
-    body.className = "dark-theme";
-    btn.innerHTML = "💡"
-  }
-  btn.onclick = () => {
-    if (body.className) {
-      body.className = "";
-      btn.innerHTML = "🌙"
-      localStorage.setItem("theme", "");
-    } else {
-      body.className = "dark-theme";
-      btn.innerHTML = "💡"
-      localStorage.setItem("theme", "dark");
+    if (localStorage.getItem("theme") === "dark") {
+        body.className = "dark-theme";
+        btn.innerHTML = "💡";
     }
-  }
-}
+    btn.onclick = () => {
+        if (body.className) {
+            body.className = "";
+            btn.innerHTML = "🌙";
+            localStorage.setItem("theme", "");
+        } else {
+            body.className = "dark-theme";
+            btn.innerHTML = "💡";
+            localStorage.setItem("theme", "dark");
+        }
+    };
+};
